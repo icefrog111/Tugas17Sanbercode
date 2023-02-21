061dcc09be7b3f3aeb26ebb479b53d8c93d398b1
icefrog111 <icefrog111@gmail.com>
2023-02-21 10:08:57 +0700

    ganti

--- seleniumdiego.py
+++ seleniumdiego.py
@@ -4,50 +4,53 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-class Test(unittest.TestCase):
+class TestLogin(unittest.TestCase): 
 
     def setUp(self):
-        self.driver = webdriver.Chrome(r'C:\Users\icefr\OneDrive\Desktop\Bootcamp SQA\Tugas17Sanbercode\chromedriver.exe')
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
 
     #login
     def login(self):
-        driver = self.driver
-        driver.get("https://www.saucedemo.com/")
+        browser = self.browser #buka web browser
+        browser.get("https://www.saucedemo.com/")
         time.sleep(3)
-        driver.find_element(By.NAME, 'user-name').send_keys('standard_user')
+        browser.find_element(By.NAME, 'user-name').send_keys('standard_user')
         time.sleep(2)
-        driver.find_element(By.NAME, 'password').send_keys('secret_sauce')
+        browser.find_element(By.NAME, 'password').send_keys('secret_sauce')
         time.sleep(2)
-        driver.find_element(By.ID,"login-button").click()
+        browser.find_element(By.ID,"login-button").click()
         time.sleep(2)
         
     #logout
     def login(self):
-        driver = self.driver
-        driver.get("https://www.saucedemo.com/")
+        browser = self.browser #buka web browser
+        browser.get("https://www.saucedemo.com/")
         time.sleep(3)
-        driver.find_element(By.NAME, 'user-name').send_keys('standard_user')
+        browser.find_element(By.NAME, 'user-name').send_keys('standard_user')
         time.sleep(2)
-        driver.find_element(By.NAME, 'password').send_keys('secret_sauce')
+        browser.find_element(By.NAME, 'password').send_keys('secret_sauce')
         time.sleep(2)
-        driver.find_element(By.ID,"login-button").click()
+        browser.find_element(By.ID,"login-button").click()
         time.sleep(2)
-        driver.find_element(By.ID,"react-burger-menu-btn").click()
+        browser.find_element(By.ID,"react-burger-menu-btn").click()
         time.sleep(2)
-        driver.find_element(By.ID,"logout_sidebar_link").click()
+        browser.find_element(By.ID,"logout_sidebar_link").click()
         time.sleep(2)
         
     #login tanpa email dan password
     def login(self):
-        driver = self.driver
-        driver.get("https://www.saucedemo.com/")
+        browser = self.browser #buka web browser
+        browser.get("https://www.saucedemo.com/")
         time.sleep(3)
-        driver.find_element(By.NAME, 'user-name').send_keys('standard_user')
+        browser.find_element(By.NAME, 'user-name').send_keys('standard_user')
         time.sleep(2)
-        driver.find_element(By.NAME, 'password').send_keys('secret_sauce')
-        time.sleep(2)   
-        driver.find_element(By.ID,"login-button").click()
+        browser.find_element(By.NAME, 'password').send_keys('secret_sauce')
+        time.sleep(2)
+        browser.find_element(By.ID,"login-button").click()
         time.sleep(2)
         
-if __name__ == '__main__':
+    def tearDown(self): 
+        self.browser.close()    
+        
+if __name__ == "__main__": 
     unittest.main()
